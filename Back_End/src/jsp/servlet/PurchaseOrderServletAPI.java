2110955b2b5ea7819005a7306eb5129063230888
Udith_Dev_Wee <daveudith@gmail.com>
2023-08-27 15:54:32 +0530

    updated

---
+++ Back_End/src/jsp/servlet/PurchaseOrderServletAPI.java
@@ -0,0 +1,9 @@
+package jsp.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/pages/purchase-order"})
+public class PurchaseOrderServletAPI extends HttpServlet {
+
+}
